fcfff30c17808781de8af88dc4f404f038d12c32
Lenovo <Lenovo@DESKTOP-M1Q9SA0>
2024-02-27 15:58:36 +0700

    Add project files.

---
+++ PERTEMUANPERTAMA/PERTEMUANPERTAMA.cpp
@@ -0,0 +1,30 @@
+// PERTEMUANPERTAMA.cpp : This file contains the 'main' function. Program execution begins and ends there.
+using namespace std;
+
+
+#include <iostream>
+
+int main()
+{
+    //Begin
+    //numeric nAlas, nTinggi, nLuas
+    //Display 'Masukkan Alas = ' 
+    //Accept nAlas
+    //Display 'Masukkan Tinggi = '
+    //Accept nTinggi
+    //Compute nLuas = nAlas*nTinggi/2
+    //Display 'Luasnya = ' + nLuas
+    //End
+
+    {
+        float nAlas, nTinggi, nLuas;
+        cout << "Masukan Alas = ";
+        cin >> nAlas;
+        cout << "Masukan Tinggi = ";
+        cin >> nTinggi;
+        nLuas = nAlas * nTinggi / 2;
+        cout << "Luasnya = " << nLuas;
+
+
+
+    }
